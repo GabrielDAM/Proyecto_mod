24afcebaa44eef5f856ff6bc50fba143d657ace5
GabrielDAM <52115160+GabrielDAM@users.noreply.github.com>
2021-02-11 16:13:01 +0100

    Initial commit

---
+++ app/src/main/java/es/gabriel/proyecto_mod/MainActivity.java
@@ -0,0 +1,18 @@
+package es.gabriel.proyecto_mod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
